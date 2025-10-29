cf8eff00cc1683e99345ab71207329c01cf21d16
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-10-29 11:05:37 +0400

    added application module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApplicationModule } from './application/application.module';
 
 @Module({
-  imports: [],
+  imports: [ApplicationModule],
   controllers: [AppController],
   providers: [AppService],
 })
