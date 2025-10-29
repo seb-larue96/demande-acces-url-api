38cdb91f0687ea9420e9ee01042a209a1bde9da0
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-10-29 15:59:56 +0400

    configured validation pipes

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { configureSwagger } from './config/swagger.config';
 import { configureGlobalInterceptors } from './config/interceptor.config';
+import { configureValidationPipes } from './config/validation-pipe.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   configureSwagger(app);
+  configureValidationPipes(app);
   configureGlobalInterceptors(app);
 
   await app.listen(process.env.PORT ?? 3000);
