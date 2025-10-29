8205a917192ce31b191fa28a6d17a8546d060b33
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-10-29 15:35:32 +0400

    configured api response interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { configureSwagger } from './config/swagger.config';
+import { configureGlobalInterceptors } from './config/interceptor.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   configureSwagger(app);
+  configureGlobalInterceptors(app);
 
   await app.listen(process.env.PORT ?? 3000);
 }
