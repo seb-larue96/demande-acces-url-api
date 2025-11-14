ce3c9ee4fdc928c4557d54a1787b512f072e5d26
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-11-14 14:47:55 +0400

    update create access request dto

--- src/application/access-request/dto/create-access-request.dto.ts
+++ src/application/access-request/dto/create-access-request.dto.ts
@@ -1,1 +1,14 @@
-export class CreateAccessRequestDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsNotEmpty } from "class-validator";
+
+export class CreateAccessRequestDto {
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    url: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    reasonToRequest: string;
+}
