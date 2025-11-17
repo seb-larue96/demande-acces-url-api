1369d62f1c4a21cd1bc7be7975537a364274eb61
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-11-17 08:01:45 +0400

    update access request module

--- src/application/access-request/access-request.module.ts
+++ src/application/access-request/access-request.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AccessRequestService } from './access-request.service';
 import { AccessRequestController } from './access-request.controller';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 @Module({
+  imports: [
+    MikroOrmModule.forFeature(['AccessRequest'])
+  ],
   controllers: [AccessRequestController],
   providers: [AccessRequestService],
 })
