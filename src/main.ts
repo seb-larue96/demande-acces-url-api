abb2153dee2e9bc46a8fca6e0cc726c93d939b05
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-11-24 16:00:38 +0400

    add cookie parser config to main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { configureSwagger } from './config/swagger.config';
 import { configureGlobalInterceptors } from './config/interceptor.config';
 import { configureValidationPipes } from './config/validation-pipe.config';
 import { configureCors } from './config/cors.config';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
   configureValidationPipes(app);
   configureGlobalInterceptors(app);
   configureCors(app);
+
+  app.use(cookieParser());
 
   await app.listen(process.env.PORT ?? 3000);
 }
