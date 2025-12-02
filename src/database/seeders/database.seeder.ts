ffef4e146c6c3d4009207123afb756f6518ab9e1
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-12-02 09:27:51 +0400

    update database seeder

--- src/database/seeders/database.seeder.ts
+++ src/database/seeders/database.seeder.ts
@@ -1,9 +1,10 @@
 import { Seeder } from "@mikro-orm/seeder";
 import { EntityManager } from "@mikro-orm/core";
 import { AccessRequestStatusSeeder } from "./access-request-status.seeder";
+import { RoleSeeder } from "./role.seeder";
 
 export class DatabaseSeeder extends Seeder {
     async run (em: EntityManager): Promise<void> {
-        await this.call(em, [AccessRequestStatusSeeder]);
+        await this.call(em, [AccessRequestStatusSeeder, RoleSeeder]);
     }
 }
