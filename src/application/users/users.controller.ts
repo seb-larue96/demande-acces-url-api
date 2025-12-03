03384c8c1a456634465541ebab3369b84ee544b7
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-12-03 12:16:46 +0400

    update user controller with roles

--- src/application/users/users.controller.ts
+++ src/application/users/users.controller.ts
@@ -3,12 +3,14 @@ import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Roles } from 'src/decorators/role.decorator';
 
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Roles('Admin')
   @Post('createUser')
   @ApiOperation({ summary: 'Create a new user' })
   @ApiResponse({ status: 201, description: 'The user has been successfully created.' })
@@ -18,6 +20,7 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
+  @Roles('Admin')
   @Get('getUsers')
   @ApiOperation({ summary: 'Get all users' })
   @ApiResponse({ status: 200, description: 'List of all users.' })
@@ -26,6 +29,7 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
+  @Roles('Guest')
   @Get('getUserById/:id')
   @ApiOperation({ summary: 'Get user by id' })
   @ApiParam({ name: 'id', type: Number, description: 'The id of the user to retrieve.' })
@@ -36,6 +40,7 @@ export class UsersController {
     return this.usersService.findOneById(id);
   }
 
+  @Roles('Guest')
   @Patch('updateUser/:id')
   @ApiOperation({ summary: 'Update existing user by id' })
   @ApiParam({ name: 'id', type: Number, description: 'The id of the user to update.' })
@@ -46,6 +51,7 @@ export class UsersController {
     return this.usersService.update(id, updateUserDto);
   }
 
+  @Roles('Admin')
   @Delete('removeUser/:id')
   @ApiOperation({ summary: 'Remove user by id' })
   @ApiParam({ name: 'id', type: Number, description: 'The id of the user to remove.' })
