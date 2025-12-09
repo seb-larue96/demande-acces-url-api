6f13b3bf3bdd261d6e9228b528ef446a7d5cf037
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-12-09 15:10:44 +0400

    add mapper for pending access request response dto

--- src/application/access-request/mapping/access-request.mapper.ts
+++ src/application/access-request/mapping/access-request.mapper.ts
@@ -1,3 +1,4 @@
+import { PendingAccessRequestResponseDto } from "../dto/pending-access-request-response.dto";
 import { AccessRequestResponseDto } from "../dto/access-request-response.dto";
 import { AccessRequest } from "../entities/access-request.entity";
 
@@ -5,10 +6,20 @@ export function mapToAccessRequestResponseDto(accessRequest: AccessRequest): Acc
     return {
         id: accessRequest.id,
         requestNumber: accessRequest.requestNumber,
-        url: accessRequest.Url,
+        url: accessRequest.url,
         requester: accessRequest.requester.name + ' ' + accessRequest.requester.surname,
         reasonToRequest: accessRequest.reasonToRequest,
         reasonToReject: accessRequest.reasonToReject,
+        requestStatus: accessRequest.requestStatus.code
+    }
+}
+
+export function mapToAccessRequestPendingResponseDto(accessRequest: AccessRequest): PendingAccessRequestResponseDto {
+    return {
+        id: accessRequest.id,
+        url: accessRequest.url,
+        requesterEmail: accessRequest.requester.email,
+        reasonToRequest: accessRequest.reasonToRequest,
         requestStatus: accessRequest.requestStatus.code
     }
 }
